0b3d08abf852bc3360a497ca3d2ba8e140a77ecc
maxusify <michalpedzik.dev@gmail.com>
2023-10-12 12:57:05 +0200

    fix: Move site metadata to a file

---
+++ src/constants/site-data.ts
@@ -0,0 +1,24 @@
+type SiteData = {
+    title: string;
+    author: string;
+    description: string;
+    keywords: string[];
+};
+
+export const siteData: SiteData = {
+    title: "maxusify.dev",
+    author: "Michał Pędzik",
+    description: "My personal site/blog/portfolio/place in the Internet.",
+    keywords: [
+        "web",
+        "dev",
+        "developer",
+        "maxusify",
+        "react",
+        "linux",
+        "typescript",
+        "javascript",
+    ],
+};
+
+export default siteData;
