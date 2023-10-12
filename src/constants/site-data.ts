828b93c9e9d09d9d0e7abd594f1a2791fd5631bc
maxusify <michalpedzik.dev@gmail.com>
2023-10-12 13:09:33 +0200

    style: Fix formatting for `src`

--- src/constants/site-data.ts
+++ src/constants/site-data.ts
@@ -1,24 +1,24 @@
 type SiteData = {
-    title: string;
-    author: string;
-    description: string;
-    keywords: string[];
-};
+  title: string
+  author: string
+  description: string
+  keywords: string[]
+}
 
 export const siteData: SiteData = {
-    title: "maxusify.dev",
-    author: "Michał Pędzik",
-    description: "My personal site/blog/portfolio/place in the Internet.",
-    keywords: [
-        "web",
-        "dev",
-        "developer",
-        "maxusify",
-        "react",
-        "linux",
-        "typescript",
-        "javascript",
-    ],
-};
+  title: 'maxusify.dev',
+  author: 'Michał Pędzik',
+  description: 'My personal site/blog/portfolio/place in the Internet.',
+  keywords: [
+    'web',
+    'dev',
+    'developer',
+    'maxusify',
+    'react',
+    'linux',
+    'typescript',
+    'javascript'
+  ]
+}
 
-export default siteData;
+export default siteData
