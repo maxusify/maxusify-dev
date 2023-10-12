0010eac38a4f301af7a86959276e4db3a923fef1
maxusify <michalpedzik.dev@gmail.com>
2023-10-12 15:46:30 +0200

    refactor: Move nav data to a file

---
+++ src/constants/nav-items.ts
@@ -0,0 +1,14 @@
+type Item = {
+  name: string
+  link: string
+  enabled?: boolean
+}
+
+export const items: Item[] = [
+  { name: 'Home', link: '/' },
+  { name: 'About Me', link: '/about' },
+  { name: 'Projects', link: '/projects', enabled: false },
+  { name: 'Contact', link: '/contact', enabled: false }
+]
+
+export default items
