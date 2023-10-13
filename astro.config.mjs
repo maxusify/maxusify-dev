71760d27938bc292e76629ba6fe7e7b675b18fdc
maxusify <michalpedzik.dev@gmail.com>
2023-10-13 11:39:32 +0200

    feat: `shadcn/ui` components

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind({ applyBaseStyles: false })],
   output: "server",
   adapter: vercel(),
 });
