d2d6765ec6f48d4a5f8edefad588a20f3240699a
maxusify <michalpedzik.dev@gmail.com>
2023-10-16 13:42:20 +0200

    refactor: Home page buttons are dynamic

---
+++ src/constants/home-buttons.ts
@@ -0,0 +1,19 @@
+type HomeButton = {
+  name: string
+  href: string
+}
+
+export const HOME_BUTTONS: HomeButton[] = [
+  {
+    name: 'My Projects',
+    href: '/portfolio'
+  },
+  {
+    name: 'My Blog',
+    href: '/blog'
+  },
+  {
+    name: 'Contact with Me',
+    href: '/contact'
+  }
+]
